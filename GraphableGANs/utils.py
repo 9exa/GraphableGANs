85570f579d7a4eef8c310e7add3c7d37718489a7
9exa <resosudarmo.wisesa@gmail.com>
2021-06-22 17:06:38 +1000

    fix inital commit

--- GraphableGANs/utils.py
+++ GraphableGANs/utils.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import tensorflow as tf
 #functions to help encode/decode/manage data
 #preferably not something that already exists in tensorflow
@@ -14,20 +14,3 @@ def multiToSimple(graphs, handleMults = "ignore"):
         for e in g.edges:
             out[-1][e[0]][e[1]] = 1
     return out
-=======
-import tensorflow as tf
-#functions to help encode/decode/manage data
-#preferably not something that already exists in tensorflow
-
-#turns a multi-edged graphs to a simple graphs (edgelist to adjmatrix)
-def multiToSimple(graphs, handleMults = "ignore"):
-    #"ignore" treats multiple connections as 1
-    #"add" counts up multiple connections
-    out = []
-    for g in graphs:
-        n = len(g)
-        out.append(list(tf.zeroes([n,n])))
-        for e in g.edges:
-            out[-1][e[0]][e[1]] = 1
-    return out
->>>>>>> 523aa83195ddb6e076d66f660f7be7dc55a96411
